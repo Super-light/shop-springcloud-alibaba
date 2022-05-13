25c2627242da47d73dfd8706e4646d3bf587eb0d
plus <xutian920516@hotmail.com>
2022-05-13 10:59:59 +0800

    [code] load balancing

--- shop-order/src/main/java/io/plus/shop/OrderStarter.java
+++ shop-order/src/main/java/io/plus/shop/OrderStarter.java
@@ -3,6 +3,7 @@ package io.plus.shop;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement(proxyTargetClass = true)
 @MapperScan(value = { "io.plus.shop.mapper" })
+@EnableDiscoveryClient
 public class OrderStarter {
     public static void main(String[] args){
         SpringApplication.run(OrderStarter.class, args);
--- shop-product/src/main/java/io/plus/shop/ProductStarter.java
+++ shop-product/src/main/java/io/plus/shop/ProductStarter.java
@@ -3,6 +3,7 @@ package io.plus.shop;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @MapperScan(value = { "io.plus.shop.product.mapper" })
 @EnableTransactionManagement(proxyTargetClass = true)
+@EnableDiscoveryClient
 public class ProductStarter {
 
     public static void main(String[] args){
